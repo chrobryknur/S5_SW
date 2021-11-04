86b891a4c320dece3603065b20fc8c2b20687c9f
Marcin Dabrowski <mdabrowskin@gmail.com>
2021-11-04 13:13:24 +0100

    Add L3 Z1, Z3 jpgs, refactor L3/Z3

--- L3/Z3/led.c
+++ L3/Z3/led.c
@@ -3,7 +3,7 @@
 
 static inline void initADC0(){
     ADMUX  |= _BV(REFS0);
-    ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
+    ADCSRA |= _BV(ADPS1);
     ADCSRA |= _BV(ADEN);
 }
 
@@ -11,102 +11,31 @@ static inline void turn_tx_and_rx_off() {
     UCSR0B &= ~_BV(RXEN0) & ~_BV(TXEN0);
 }
 
-void delay(uint16_t us){
+static inline void delay(uint16_t us){
     while(us--) _delay_us(1);
 }
+
+static inline void init_led(){
+    DDRD = 0xFF;
+    PORTD = 0;
+}
+
 int main(){
     initADC0();
     turn_tx_and_rx_off();
-    DDRD = 0xFF;
+    init_led();
 
     uint16_t adcValue;
 
     while(1){
         ADCSRA |= _BV(ADSC);
         loop_until_bit_is_clear(ADCSRA, ADSC);
-        adcValue = ADC;
+        adcValue = ADC >> 7;
 
         PORTD = _BV(PD2);
-        delay(1 << (adcValue/100));
-        /* switch(adcValue / 100){ */
-        /*     case 0: */
-        /*         delay(1 << 0); */
-        /*         break; */
-        /*     case 1: */
-        /*         delay(1 << 1); */
-        /*         break; */
-        /*     case 2: */
-        /*         delay(1 << 2); */
-        /*         break; */
-        /*     case 3: */
-        /*         delay(1 << 3); */
-        /*         break; */
-        /*     case 4: */
-        /*         delay(1 << 4); */
-        /*         break; */
-        /*     case 5: */
-        /*         delay(1 << 5); */
-        /*         break; */
-        /*     case 6: */
-        /*         delay(1 << 6); */
-        /*         break; */
-        /*     case 7: */
-        /*         delay(1 << 7); */
-        /*         break; */
-        /*     case 8: */
-        /*         delay(1 << 8); */
-        /*         break; */
-        /*     case 9: */
-        /*         delay(1 << 9); */
-        /*         break; */
-        /*     case 10: */
-        /*         delay(1 << 10); */
-        /*         break; */
-        /*     case 11: */
-        /*         delay(1 << 11); */
-        /*         break; */
-        /*     case 12: */
-        /*         delay(1 << 12); */
-        /*         break; */
-        /*     default: */
-        /*         delay(1 << 13); */
-        /*         break; */
-        /* } */
+        delay(1 << (adcValue));
+
         PORTD = 0;
-        delay(1 << (10 - adcValue/100));
-        /* switch(adcValue/100){ */
-        /*     case 0: */
-        /*         delay(1 << 10); */
-        /*         break; */
-        /*     case 1: */
-        /*         delay(1 << 9); */
-        /*         break; */
-        /*     case 2: */
-        /*         delay(1 << 8); */
-        /*         break; */
-        /*     case 3: */
-        /*         delay(1 << 7); */
-        /*         break; */
-        /*     case 4: */
-        /*         delay(1 << 6); */
-        /*         break; */
-        /*     case 5: */
-        /*         delay(1 << 5); */
-        /*         break; */
-        /*     case 6: */
-        /*         delay(1 << 4); */
-        /*         break; */
-        /*     case 7: */
-        /*         delay(1 << 3); */
-        /*         break; */
-        /*     case 8: */
-        /*         delay(1 << 2); */
-        /*         break; */
-        /*     case 9: */
-        /*         delay(1 << 1); */
-        /*         break; */
-        /*     default: */
-        /*         break; */
-        /* } */
+        delay(1 << (10 - adcValue));
     }
 }
