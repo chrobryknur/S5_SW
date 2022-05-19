e6df13c6add46437c0e7f1b6aecbd8d337814a69
Marcin Dąbrowski <mdabrowskin@gmail.com>
2022-05-19 13:43:09 +0200

    Add Z2/L10

---
+++ L10/Z2/engine.c
@@ -0,0 +1,69 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+
+#define BAUD 9600
+#define UBRR_VALUE ((F_CPU)/16/(BAUD)-1)
+
+static inline void initADC0(){
+    ADMUX  |= _BV(REFS0);
+    ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
+    ADCSRA |= _BV(ADEN);
+    ADCSRA |= _BV(ADIE);
+}
+
+void timer1_init()
+{
+  // ustaw tryb licznika
+  // COM1A = 10   -- non-inverting mode
+  // WGM1  = 1110 -- fast PWM top=ICR1
+  // CS1   = 010 -- prescaler 8
+  // ICR1  = 199
+  // częstotliwość 16e6/(8*(1+199)) = 1kHz
+  // wzór: datasheet 20.12.3 str. 164
+  ICR1 = 199;
+  TCCR1A = _BV(COM1A1) | _BV(WGM13);
+  TCCR1B = _BV(WGM12) | _BV(WGM13) | _BV(CS11);
+  // ustaw pin OC1A (PB1) jako wyjście
+  DDRB |= _BV(PB1);
+}
+
+void uart_init()
+{
+  UBRR0 = UBRR_VALUE;
+  UCSR0A = 0;
+  UCSR0B = _BV(RXEN0) | _BV(TXEN0);
+  UCSR0C = _BV(UCSZ00) | _BV(UCSZ01);
+}
+
+int uart_transmit(char data, FILE *stream)
+{
+  while(!(UCSR0A & _BV(UDRE0)));
+  UDR0 = data;
+  return 0;
+}
+
+int uart_receive(FILE *stream)
+{
+  while (!(UCSR0A & _BV(RXC0)));
+  return UDR0;
+}
+
+FILE uart_file;
+
+int main(){
+    uart_init();
+    fdev_setup_stream(&uart_file, uart_transmit, uart_receive, _FDEV_SETUP_RW);
+    stdin = stdout = stderr = &uart_file;
+
+    initADC0();
+    timer1_init();
+
+
+    while(1){
+        ADCSRA |= _BV(ADSC);
+        loop_until_bit_is_clear(ADCSRA, ADSC);
+        uint16_t adcValue = ADC;
+        OCR1A = (adcValue >> 2);
+    }
+}
